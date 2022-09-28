48a7929622a494f1c2cbae816ad3a470c39d7667
Pedro Neto <pedromneto97@gmail.com>
2022-09-28 19:35:50 -0300

    feat: add actions

---
+++ lib/src/presentation/home/widgets/converter_actions.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubit/file_convert_cubit/file_convert_cubit.dart';
+import '../cubit/file_selector_cubit/file_selector_cubit.dart';
+import '../cubit/parameter_cubit/parameter_cubit.dart';
+
+class ConverterActions extends StatelessWidget {
+  const ConverterActions({super.key});
+
+  void clear(BuildContext context) {
+    BlocProvider.of<FileSelectorCubit>(context).clear();
+    BlocProvider.of<FileConvertCubit>(context).clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        BlocSelector<FileSelectorCubit, FileSelectorState, List<File>>(
+          selector: (state) => state is FileSelectorSelected ? state.selectedFiles : const [],
+          builder: (context, state) => ElevatedButton(
+            onPressed: () => BlocProvider.of<FileConvertCubit>(context).convertFiles(
+              state,
+              BlocProvider.of<ParameterCubit>(context).state,
+            ),
+            child: const Text('Convert'),
+          ),
+        ),
+        const SizedBox(height: 16.0),
+        OutlinedButton(
+          onPressed: () => clear(context),
+          child: const Text('Clear file'),
+        ),
+      ],
+    );
+  }
+}
